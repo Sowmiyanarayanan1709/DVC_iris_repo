f2e135bf97fe7f417efc26b3fda885aec7a2278f
Sowmiyanarayanan S <sowmiyanarayanan.s@cat.com>
2023-03-08 19:32:22 +0530

    model version 4

--- py_code.py
+++ py_code.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 import pickle
 import subprocess
 
-df = pd.read_csv("C:\\Users\\s123\\Desktop\\Iris_project\\Iris.csv")
+df = pd.read_csv("C:\\Users\\s123\\Desktop\\DVC_iris_repo\\Iris.csv")
 
 flower_mapping = {'Iris-setosa' : 0, 'Iris-versicolor' : 1, 'Iris-virginica' : 2 }
 df["Species"] = df["Species"].map(flower_mapping)
@@ -35,7 +35,7 @@ df_rf = df_rf.iloc[:,[1,3,8,10]]
 df_rf["Accuracy"]  = accuracy
 df_rf.insert(0,'model_name','Random Forest_'+str(git_count))
 df_rf.insert(len(df_rf.columns), 'Number of rows', len(df) )
-df_rf.to_csv("C:\\Users\\s123\\Desktop\\Iris_project\\parameters.csv",mode = 'a', index=False, header = False)
+df_rf.to_csv("C:\\Users\\s123\\Desktop\\DVC_iris_repo\\parameters.csv",mode = 'a', index=False, header = False)
 
 model_dt = DecisionTreeClassifier(ccp_alpha=0.1, min_samples_split=10)
 model_dt.fit(X_train,y_train)
@@ -51,6 +51,6 @@ df_dt = df_dt.iloc[:,[0,2,6,8]]
 df_dt["Accuracy"]  = accuracy
 df_dt.insert(0,'model_name','Decision Tree_'+str(git_count))
 df_dt.insert(len(df_dt.columns), 'Number of rows', len(df))
-df_dt.to_csv("C:\\Users\\s123\\Desktop\\Iris_project\\parameters.csv",mode = 'a', index=False, header = False)
+df_dt.to_csv("C:\\Users\\s123\\Desktop\\DVC_iris_repo\\parameters.csv",mode = 'a', index=False, header = False)
 
 
